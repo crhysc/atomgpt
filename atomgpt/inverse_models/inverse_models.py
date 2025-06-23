508ee93fa3f48b38f3aa92fbf88a1c56112a266e
Campbell <crc00042@mix.wvu.edu>
2025-06-23 10:59:53 -0600

    add hp_cfg_path field

--- atomgpt/inverse_models/inverse_models.py
+++ atomgpt/inverse_models/inverse_models.py
@@ -91,6 +91,7 @@ class TrainingPropConfig(BaseSettings):
     # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
     load_in_4bit: bool = True
     # True  # Use 4bit quantization to reduce memory usage. Can be False.
+    hp_cfg_path: Optional[str] = "./hp_search_config.json"
     instruction: str = "Below is a description of a superconductor material."
     alpaca_prompt: str = (
         "### Instruction:\n{}\n### Input:\n{}\n### Output:\n{}"
